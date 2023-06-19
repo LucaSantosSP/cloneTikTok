f14e1abb2406448134af20847b5bfc9c564b83c0
LucaSantosSp <lucas-santos20.17@hotmail.com>
2023-06-18 23:25:09 -0300

    feat: adicionado sidebar e preparado ambiente para DB

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video/>
-        <Video/>
-        <Video/>
+        <Video 
+          likes={100} 
+          messages={25} 
+          shares={12}
+          name="bugas.sp"
+          description="Brecker o goleiro"
+          music="Música épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video
+          likes={15} 
+          messages={0} 
+          shares={1}
+          name="pedro0025"
+          description="Bird olhando para a câmera"
+          music="Música clap your hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+        />
       </div>
     </div>
   );
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react'
 import VideoFooter from "./components/footer/VideoFooter"
+import VideoSidebar from './components/sidebar/VideoSidebar';
 import "./video.css"
 
-function Video() {
+function Video({likes, messages, shares, name, description, music, url}) {
     const videoRef = useRef(null);
     const [play, setPlay] = useState(false);
 
@@ -24,12 +25,20 @@ return (
         ref={videoRef} 
         onClick={handdleStart}
         loop 
-        src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'>
+        src={url}>
         </video>
 
-        { /* Side Bar */ }
+        <VideoSidebar 
+            likes={likes}
+            messages={messages}
+            shares={shares}
+        />
 
-        <VideoFooter />
+        <VideoFooter 
+            name={name} 
+            description={description} 
+            music={music}
+        />
     </div>
   );
 }
